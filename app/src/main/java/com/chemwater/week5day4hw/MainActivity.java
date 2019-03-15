3e98e0ee691f8550da4cb5804ccbbf29828d863d
Ubeni ShepsuMa'at <dariuswalker878@gmail.com>
2019-03-15 08:53:42 -0400

    Initial commit

---
+++ app/src/main/java/com/chemwater/week5day4hw/MainActivity.java
@@ -0,0 +1,26 @@
+package com.chemwater.week5day4hw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+/*
+1.  Make a app with 2 activities, one is a login
+and one is a landing activity for the login
+
+
+2.  Use Facebook authentication to achieve the logging in
+
+
+3.  Implement firebase cloud messaging in your app.  Add a screenshot of
+the push notification displaying on the device.
+ */
